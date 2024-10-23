111e206b913c473ce7ec52a2ca47071e4248ab8b
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2024-10-24 00:06:51 +0200

    Created the information page and added imports

--- SplashScreen.tsx
+++ SplashScreen.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Image } from 'react-native';
 import { Button } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
   return (
---
+++ screens/informationScreen.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
+import { Button } from 'react-native';
+
+
+
+export default function informationScreen() {
+
+
+
+
+}
